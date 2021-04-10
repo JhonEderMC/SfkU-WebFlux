bdaeee5fc6d0521f380c5037e43f80d8afd18d55
jhon mc <kgbxblackx@gmail.com>
2021-04-10 00:36:17 -0500

    Se crea el controlador para la people, respondera a las peticiones rest.
    se crea el metodo getPeople para retornar el listado de personas.

---
+++ src/main/java/com/example/controller/PersonController.java
@@ -0,0 +1,22 @@
+package com.example.controller;
+
+import com.example.model.Person;
+import com.example.service.PersonServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    PersonServiceInterface service;
+
+    @GetMapping
+    public Flux<Person> getPeople(){
+     return  service.findAll();
+    }
+}
