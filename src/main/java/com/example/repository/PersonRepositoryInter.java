afb59504e5a6a47c7f77ceb5e7e5f0793fd8a3bc
jhon mc <kgbxblackx@gmail.com>
2021-04-11 11:52:20 -0500

    Se agrega saveALL.
    Correspondara para guardar una listad e personas.

--- src/main/java/com/example/repository/PersonRepositoryInter.java
+++ src/main/java/com/example/repository/PersonRepositoryInter.java
@@ -4,12 +4,15 @@ import com.example.model.Person;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+
 public interface PersonRepositoryInter {
 
     Flux<Person> findAll();
     Mono<Person> findById(Integer personId);
     Flux<Person> findByName(String name);
     Mono<Person> save(Person person);
+    Flux<Person> saveAll(List<Person> personList);
     void delete(Integer personId);
 
 }
