c0c3e827e77ce0299ad0f59136be181bb77c8ca7
jhon mc <kgbxblackx@gmail.com>
2021-04-11 15:57:24 -0500

    Se crea un punto de entrada para solicitar las estadisticas de la base de datos de personas.

--- src/main/java/com/example/controller/PersonController.java
+++ src/main/java/com/example/controller/PersonController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.IntSummaryStatistics;
 import java.util.List;
 
 @RestController
@@ -52,7 +53,12 @@ public class PersonController {
 
     @GetMapping("/age")
     public Flux<Person> greatAndLessPersonAge(){
-        return service.greatAndLesPersonAge();
+        return service.greatAndLessPersonAge();
+    }
+
+    @GetMapping("/statistics")
+    public Mono<IntSummaryStatistics> statistics(){
+        return service.statistics();
     }
 
 }
