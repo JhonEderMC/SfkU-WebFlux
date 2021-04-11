7d0e6d445cd9770d35f26b926b3d69a092f4e7d1
jhon mc <kgbxblackx@gmail.com>
2021-04-10 19:40:43 -0500

    Se  define buscar por nombre.

--- src/main/java/com/example/repository/PersonRepositoryInter.java
+++ src/main/java/com/example/repository/PersonRepositoryInter.java
@@ -8,6 +8,7 @@ public interface PersonRepositoryInter {
 
     Flux<Person> findAll();
     Mono<Person> findById(Integer personId);
+    Flux<Person> findByName(String name);
     Mono<Person> save(Person person);
     void delete(Integer personId);
 
