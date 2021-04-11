2b6381e0383ee55f146b49176f83f36a5d209836
jhon mc <kgbxblackx@gmail.com>
2021-04-10 23:03:16 -0500

    Se crea el controlador saveperson para guardar una persona.

--- src/main/java/com/example/controller/PersonController.java
+++ src/main/java/com/example/controller/PersonController.java
@@ -3,10 +3,7 @@ package com.example.controller;
 import com.example.model.Person;
 import com.example.service.PersonServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -30,6 +27,11 @@ public class PersonController {
     @GetMapping("/name/{name}")
     public Flux<Person> getPeopleByName(@PathVariable String name){
         return service.findByName(name);
+    }
+
+    @PostMapping
+    public Mono<Person> savePerson(@RequestBody Person person){
+        return service.save(person);
     }
 
 }
